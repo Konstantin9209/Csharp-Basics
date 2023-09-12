9b81d3079014fa3c261ba1b69e41081cb59cb7d6
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-09-13 00:48:34 +0100

    Add files via upload

---
+++ T04/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace T04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pagesCount = int.Parse(Console.ReadLine());
+            int pages = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            int hours = pagesCount / (pages * days);
+            Console.WriteLine(hours);
+        }
+    }
+}
