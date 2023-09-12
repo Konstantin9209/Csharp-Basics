8e1763ddc723108e74a88f754999c7c668b83d3b
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-09-13 00:50:32 +0100

    Add files via upload

---
+++ T06/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace T06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nylon = int.Parse(Console.ReadLine());
+            int paint = int.Parse(Console.ReadLine());
+            int thinner = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+
+            double priceForNylon = (nylon + 2) * 1.50;
+            double priceForPaint = (paint + paint * 0.10) * 14.50;
+            double priceForThinner = thinner * 5;
+            double materialsPrice = priceForNylon + priceForPaint + priceForThinner + 0.40;
+            double workersPrice = (materialsPrice * 0.30) * hours;
+
+            double totalSum = materialsPrice + workersPrice;
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
