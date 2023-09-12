5305cda7e21447a0e3077b5ea6d8df6f637486ab
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-09-13 00:51:29 +0100

    Add files via upload

---
+++ T08/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace T08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tax = double.Parse(Console.ReadLine());
+
+            double sneakersPrice = tax - (tax * 0.40);
+            double uniformPrice = sneakersPrice - (sneakersPrice * 0.20);
+            double ballPrice = uniformPrice / 4;
+            double accessories = ballPrice / 5;
+
+            double totalPrice = tax + sneakersPrice + uniformPrice + ballPrice + accessories;
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
