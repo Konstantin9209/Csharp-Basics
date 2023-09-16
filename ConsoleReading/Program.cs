a54db0a06299015ed298313d100237eb5339b1be
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-09-16 09:20:02 +0200

    Add files via upload

---
+++ ConsoleReading/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleReading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         string input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
