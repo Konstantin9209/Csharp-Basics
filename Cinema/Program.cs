6c549bfc1847d383def5e7824b99cece1afb5a96
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-09-16 09:14:36 +0200

    Add files via upload

---
+++ Cinema/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ticketType = Console.ReadLine();
+            int rowCount = int.Parse(Console.ReadLine());
+            int columnCount = int.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            switch (ticketType)
+            {
+                case "Premiere":
+                    price = rowCount * columnCount * 12.00;
+                    break;
+                case "Normal":
+                    price = rowCount * columnCount * 7.50;
+                    break;
+                case "Discount":
+                    price = rowCount * columnCount * 5.00;
+                    break;
+            }
+
+            Console.WriteLine($"{price:F2} leva");
+        }
+    }
+}
