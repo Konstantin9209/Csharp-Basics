9ff9434cbc7b54708d904581ac1f8eed0c32b91a
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-09-22 00:04:19 +0100

    Add files via upload

---
+++ Moving/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        int width = int.Parse(Console.ReadLine());
+        int lenght = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+
+            int boxes = width * lenght * height;
+
+            while (boxes >= 0)
+            {
+                String input = Console.ReadLine();
+                if (input == "Done")
+                    break;
+
+                boxes -= int.Parse(input);
+            }
+
+            if (boxes >= 0)
+                Console.WriteLine($"{boxes} Cubic meters left.");
+            else
+                Console.WriteLine($"No more free space! You need {-boxes} Cubic meters more.");
+        }
+    }
+}
