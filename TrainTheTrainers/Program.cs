20409b8522e1e15c0d570880bd36205523a97930
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-09-22 15:24:06 +0100

    Add files via upload

---
+++ TrainTheTrainers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TrainTheTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryMembers = int.Parse(Console.ReadLine());
+
+            double totalGradesSum = 0;
+            int presentationCount = 0;
+
+            while (true)
+            {
+                string presName = Console.ReadLine();
+
+                if (presName == "Finish")
+                    break;
+
+                presentationCount++;
+                double presentationAverage = 0;
+
+                for (int juryMember = 1; juryMember <= juryMembers; juryMember++)
+                {
+                    double curGrade = double.Parse(Console.ReadLine());
+                    presentationAverage += curGrade;
+                    totalGradesSum += curGrade;
+                }
+
+                Console.WriteLine($"{presName} - {presentationAverage / juryMembers:f2}.");
+            }
+
+            Console.WriteLine($"Student's final assessment is {totalGradesSum / (presentationCount * juryMembers):f2}");
+        }
+    }
+}
