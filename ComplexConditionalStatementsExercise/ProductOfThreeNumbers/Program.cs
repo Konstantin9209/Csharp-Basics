7af83d6b101ee399d666f2f7353d695cb3338710
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-10-03 00:10:47 +0100

    Add files via upload

---
+++ ComplexConditionalStatementsExercise/ProductOfThreeNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.Design;
+
+namespace ProductOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+
+            if (num1 == 0 || num2 == 0 || num3 == 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else if (num1 > 0 && num2 > 0 && num3 > 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else if (num1 > 0 && num2 > 0 && num3 < 0)
+            {
+                Console.WriteLine("negative");
+            }
+            else if (num1 > 0 && num2 < 0 && num3 < 0)
+            {
+                Console.WriteLine("positive");
+            }
+
+            else if (num1 < 0 && num2 > 0 && num3 > 0)
+            {
+                Console.WriteLine("negative");
+            }
+
+            else if (num1 < 0 && num2 > 0 && num3 < 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else if (num1 < 0 && num2 < 0 && num3 > 0)
+            {
+                Console.WriteLine("positive");
+            }
+
+            else if (num1 < 0 && num2 < 0 && num3 < 0)
+            {
+                Console.WriteLine("negative");
+            }
+        }
+    }
+}
+
