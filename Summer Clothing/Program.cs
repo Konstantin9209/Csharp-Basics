fc7a83580727bb9a3f98f60b89f342fc760cce21
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-10-02 13:41:16 +0100

    Update Program.cs

--- Summer Clothing/Program.cs
+++ Summer Clothing/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int temp = int.Parse(Console.ReadLine());
+            string dayTime = Console.ReadLine();
+
+            string outfit = "";
+            string shoes = "";
+
+            if (dayTime == "Morning")
+            {
+                if (temp >= 10 && temp <= 18)
+                {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                else if (temp > 18 && temp <= 24)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (temp >= 25)
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+            }
+            else if (dayTime == "Afternoon")
+            {
+                if (temp >= 10 && temp <= 18)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (temp > 18 && temp <= 24)
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (temp >= 25)
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+            }
+            else if (dayTime == "Evening")
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+
+            Console.WriteLine($"It's {temp} degrees, get your {outfit} and {shoes}.");
         }
     }
 }
