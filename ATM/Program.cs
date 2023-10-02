2ef0a07cc4985f7584807973a2771ffd5fbfa03f
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-10-02 13:58:55 +0100

    Add files via upload

---
+++ ATM/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        int balance = int.Parse(Console.ReadLine());
+            int withdraw = int.Parse(Console.ReadLine());
+            int limit = int.Parse(Console.ReadLine());
+
+            if (limit < withdraw)
+            {
+                Console.WriteLine("The limit was exceeded.");
+            }
+            else if (balance < withdraw)
+            {
+                Console.WriteLine("Insufficient availability.");
+            }
+            else
+
+                Console.WriteLine("The withdraw was successful.");
+            }
+    }
+}
