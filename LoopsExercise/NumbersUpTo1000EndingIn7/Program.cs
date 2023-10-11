0e2065a9270409262fb874304833e27b9dc12634
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-10-12 00:19:55 +0100

    Arranging folders

---
+++ LoopsExercise/NumbersUpTo1000EndingIn7/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace NumbersUpTo1000EndingIn7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 997;  i++) 
+            {
+            if(i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+                }
+    }
+}
---
+++ LoopsExercise/Vacation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyNeeded = double.Parse(Console.ReadLine());
+            double moneyAvailable = double.Parse(Console.ReadLine());
+            int totalDays = 0;
+            int spendingDays = 0;
+
+            while (moneyAvailable < moneyNeeded && spendingDays < 5)
+            {
+                totalDays++;
+                string type = Console.ReadLine();
+                double amount = double.Parse(Console.ReadLine());
+
+                if (type == "spend")
+                {
+                    moneyAvailable -= amount;
+                    if (moneyAvailable < 0)
+                        moneyAvailable = 0;
+                    spendingDays++;
+                }
+                else
+                {
+                    spendingDays = 0;
+                    moneyAvailable += amount;
+                }
+            }
+
+            if (spendingDays >= 5)
+            {
+                Console.WriteLine("You can't save the money.");
+                Console.WriteLine(totalDays);
+            }
+            else if (moneyAvailable >= moneyNeeded)
+                Console.WriteLine($"You saved the money for {totalDays} days.");
+        }
+    }
+}
