582e85d5b4d26acc62198567e8ac5ee9a024bd35
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-10-12 00:24:26 +0100

    Added nested loops and arranging folders

---
+++ NestedLoops/AccountBalance/Program.cs
@@ -0,0 +1,24 @@
+﻿double balance = 0;
+
+while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input == "End")
+    {
+        break;
+    }
+    double amount = double.Parse(input);
+    if (amount > 0)
+    {
+        balance += amount;
+        Console.WriteLine($"Increase: {amount:f2}");
+    }
+    else if (amount < 0)
+    {
+        balance += amount;
+        Console.WriteLine($"Decrease: {Math.Abs(amount):f2}");
+    }
+
+}
+Console.WriteLine($"Balance: {balance:f2}");
---
+++ NestedLoops/BiggestNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BiggestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         int countNumbers  = int.Parse(Console.ReadLine());
+            int maxNumber = int.MinValue;
+            for (int number = 1; number <= countNumbers; number++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                if (value > maxNumber)
+                {
+                    maxNumber = value;
+                }
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
