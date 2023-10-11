122669814e3d3b07e836ca466eaf174713cb1ca9
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-10-12 00:21:32 +0100

    Arranging folders

---
+++ NestedConditionalStatements/Summer Clothing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Summer_Clothing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
