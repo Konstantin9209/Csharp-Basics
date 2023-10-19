b47d0bb1bcf546ddda1a1924ab8208c0c490ce38
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-10-19 12:26:42 +0300

    Add Arrays,Lists, Nested loops and Methods

---
+++ Lists/Change List/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+while (true)
+{
+    string command = Console.ReadLine();
+
+    if (command == "end")
+    {
+        break;
+    }
+
+    string[] tokens = command.Split();
+    string action = tokens[0];
+
+    if (action == "Delete")
+    {
+        int element = int.Parse(tokens[1]);
+        numbers.RemoveAll(x => x == element);
+    }
+    else if (action == "Insert")
+    {
+        int element = int.Parse(tokens[1]);
+        int position = int.Parse(tokens[2]);
+        numbers.Insert(position, element);
+    }
+}
+
+Console.WriteLine(string.Join(" ", numbers));
+ 
---
+++ Lists/List Manipulation Advanced/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+string input;
+while ((input = Console.ReadLine()) != "end")
+{
+    string[] commandParts = input.Split(" ");
+    string commandName = commandParts[0];
+
+    if (commandName == "Contains")
+    {
+        int containedNumber = int.Parse(commandParts[1]);
+        if (numbers.Contains(containedNumber))
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No such number");
+        }
+    }
+    else if (commandName == "PrintEven")
+    {
+        Console.WriteLine(string.Join(" ", numbers.Where(n => n % 2 == 0)));
+    }
+    else if (commandName == "PrintOdd")
+    {
+        Console.WriteLine(string.Join(" ", numbers.Where(n => n % 2 != 0)));
+    }
+    else if (commandName == "GetSum")
+    {
+        Console.WriteLine(numbers.Sum());
+    }
+    else if (commandName == "Filter")
+    {
+        string condition = commandParts[1];
+        int filterNumber = int.Parse(commandParts[2]);
+
+        List<int> filteredNumbers = new List<int>();
+
+        foreach (int num in numbers)
+        {
+            if (condition == "<" && num < filterNumber)
+            {
+                filteredNumbers.Add(num);
+            }
+            else if (condition == ">" && num > filterNumber)
+            {
+                filteredNumbers.Add(num);
+            }
+            else if (condition == "<=" && num <= filterNumber)
+            {
+                filteredNumbers.Add(num);
+            }
+            else if (condition == ">=" && num >= filterNumber)
+            {
+                filteredNumbers.Add(num);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", filteredNumbers));
+    }
+}
+
+Console.WriteLine(string.Join(" ", numbers));
