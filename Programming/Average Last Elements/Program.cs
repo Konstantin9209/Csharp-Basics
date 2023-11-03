520ccb913ad576d91c5690f5937c84871abe77ee
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-03 09:52:55 +0000

    add 2 new programs

---
+++ Programming/Average Last Elements/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int n = int.Parse(Console.ReadLine());
+
+if (n > 0 && n <= numbers.Length)
+{
+    double average = numbers.Skip(numbers.Length - n).Take(n).Average();
+    Console.WriteLine($"{average:F2}");
+}
---
+++ Programming/English Name of Each Digit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+string input = Console.ReadLine();
+
+for (int i = input.Length - 1; i >= 0; i--)
+{
+    char digit = input[i];
+    Console.WriteLine(NumberToWord(digit));
+}
+
+string NumberToWord(char digit)
+{
+    switch (digit)
+    {
+        case '0': return "zero";
+        case '1': return "one";
+        case '2': return "two";
+        case '3': return "three";
+        case '4': return "four";
+        case '5': return "five";
+        case '6': return "six";
+        case '7': return "seven";
+        case '8': return "eight";
+        case '9': return "nine";
+        default: return "Invalid digit";
+    }
+}
