215924e2ca14d66875250899c77158cb80be0502
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-03 09:51:36 +0000

    Automation testing

---
+++ Automation Testing/03-Extract-File-Resources/03-Extract-File-Resources/TestApp.Tests/ExtractFileTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class ExtractFileTests
+{
+    [Test]
+    public void Test_GetFile_NullPath_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => ExtractFile.GetFile(null));
+    }
+
+    [Test]
+    public void Test_GetFile_EmptyPath_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => ExtractFile.GetFile(string.Empty));
+    }
+
+    [Test]
+    public void Test_GetFile_ValidPath_ReturnsFileNameAndExtension()
+    {
+        string result = ExtractFile.GetFile(@"C:\Folder\file.txt");
+        Assert.AreEqual("File name: file\nFile extension: txt", result);
+    }
+
+    [Test]
+    public void Test_GetFile_PathWithNoExtension_ReturnsFileNameOnly()
+    {
+        string result = ExtractFile.GetFile(@"C:\Folder\file");
+        Assert.AreEqual("File name: file", result);
+    }
+
+    [Test]
+    public void Test_GetFile_PathWithSpecialCharacters_ReturnsFileNameAndExtension()
+    {
+        string result = ExtractFile.GetFile(@"C:\Folder\my#file.name.txt");
+        Assert.AreEqual("File name: my#file\nFile extension: name", result);
+    }
+}
---
+++ Automation Testing/04-Drum-Set-Resources/04-Drum-Set-Resources/TestApp.Tests/DrumSetTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace TestApp.Tests;
+
+public class DrumSetTests
+{
+    [Test]
+    public void Test_Drum_TerminateCommandNotGiven_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => DrumSet.Drum(100m, new List<int> { 7, 8 }, new List<string> { "10" }));
+    }
+
+    [Test]
+    public void Test_Drum_StringGivenAsCommand_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => DrumSet.Drum(100m, new List<int> { 7, 8 }, new List<string> { "no number", "Hit it again, Gabsy!" }));
+    }
+
+    [Test]
+    public void Test_Drum_ReturnsCorrectQualityAndAmount()
+    {
+        string result = DrumSet.Drum(100m, new List<int> { 7, 8 }, new List<string> { "5", "Hit it again, Gabsy!" });
+        Assert.AreEqual("2 3\nGabsy has 100.00lv.", result);
+    }
+
+    [Test]
+    public void Test_Drum_BalanceZero_WithOneDrumLeftOver_ReturnsCorrectQualityAndAmount()
+    {
+        string result = DrumSet.Drum(24m, new List<int> { 7, 8 }, new List<string> { "10", "Hit it again, Gabsy!" });
+        Assert.AreEqual("7\nGabsy has 3.00lv.", result);
+    }
+
+
+    [Test]
+    public void Test_Drum_NotEnoughBalance_RemovesAllDrums_ReturnsCorrectQualityAndAmount()
+    {
+        string result = DrumSet.Drum(5m, new List<int> { 7, 8 }, new List<string> { "10", "Hit it again, Gabsy!" });
+        Assert.AreEqual("\nGabsy has 5.00lv.", result);
+    }
+}
