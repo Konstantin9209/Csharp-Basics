992b21fc826776deaed76f824ba4e35dd8abdb55
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-05 23:49:20 +0000

    Tasks 1 and 2 from exam

---
+++ Exam/Task 1/Program.cs
@@ -0,0 +1,8 @@
+﻿int count = int.Parse(Console.ReadLine());
+double grades = 0;
+for(int i = 0; i < count; i++)
+{
+    grades += double.Parse(Console.ReadLine());
+}
+double averageGrade = grades / count;
+Console.WriteLine($"{averageGrade:F2}");
---
+++ Exam/Task 2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numbs = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int rotations = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rotations; i++)
+{
+    int lastElement = numbs[numbs.Length - 1];
+    for (int j = numbs.Length - 1; j > 0; j--)
+    {
+        numbs[j] = numbs[j - 1];
+    }
+    numbs[0] = lastElement; 
+}
+
+Console.WriteLine(string.Join(", ", numbs));
