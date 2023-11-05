f56cf8cb854cc940778dcb7ca7a14bf9c869c3d1
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-05 23:53:23 +0000

    Automation testing

---
+++ 03. Unit Testing Characters Range_Ресурси/Resources/TestApp.Tests/CharacterRangeTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class CharacterRangeTests
+{
+    [Test]
+    public void Test_GetRange_WithAAndBInOrder_ReturnsEmptyString()
+    {
+        char firstChar = 'a';
+        char secondChar = 'b';
+        string expected = "";
+
+        string result = CharacterRange.GetRange(firstChar, secondChar);
+
+            Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_GetRange_WithBAndAInOrder_ReturnsEmptyString()
+    {
+        char firstChar = 'a';
+        char secondChar = 'b';
+        string expected = "";
+
+        string result = CharacterRange.GetRange(secondChar, firstChar);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_GetRange_WithAAndCInOrder_ReturnsB()
+    {
+        char firstChar = 'a';
+        char secondChar = 'c';
+        string expected = "b";
+
+        string result = CharacterRange.GetRange(firstChar, secondChar);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_GetRange_WithDAndGInOrder_Returns_E_F()
+    {
+        char firstChar = 'd';
+        char secondChar = 'g';
+        string expected = "e f";
+
+        string result = CharacterRange.GetRange(firstChar, secondChar);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_GetRange_WithXAndZInOrder_Returns_Y()
+    {
+        char firstChar = 'x';
+        char secondChar = 'z';
+        string expected = "y";
+
+        string result = CharacterRange.GetRange(firstChar, secondChar);
+
+        Assert.AreEqual(expected, result);
+    }
+}
---
+++ 04. Unit Testing Balanced Brackets_Ресурси/Resources/TestApp.Tests/BalancedBracketsTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class BalancedBracketsTests
+{
+    [Test]
+    public void Test_IsBalanced_EmptyInput_ShouldReturnTrue()
+    {
+        string[] input = Array.Empty<string>();
+        bool expected = true;
+
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_IsBalanced_BalancedBrackets_ShouldReturnTrue()
+    {
+        string[] input = new string[] { "(", ")" };
+        bool expected = true;
+
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_IsBalanced_UnbalancedBrackets_ShouldReturnFalse()
+    {
+        string[] input = new string[] { ")", ")" };
+        bool expected = false;
+
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_IsBalanced_MoreClosingBrackets_ShouldReturnFalse()
+    {
+        string[] input = new string[] { "(", ")", ")", ")" };
+        bool expected = false;
+
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_IsBalanced_NoClosingBrackets_ShouldReturnFalse()
+    {
+        string[] input = new string[] { "(" };
+        bool expected = false;
+
+        bool result = BalancedBrackets.IsBalanced(input);
+
+        Assert.AreEqual(expected, result);
+    }
+}
